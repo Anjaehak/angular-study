d18928a14b3edee405ca9b7b94eb01432fe86596
Anjh <anjh3325@gmail.com>
2023-09-11 14:08:50 +0900

    관광콘텐츠 사진

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -3,6 +3,12 @@ import { BannerComponent } from '../banner/banner.component';
 import { CardComponent, Product, ProductType } from '../card/card.component';
 import { CommonModule } from '@angular/common';
 
+export type Content = {
+  image: string;
+  title: string;
+  subTitle: string;
+};
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
@@ -15,14 +21,14 @@ export default class IndexPage {
     {
       type: ProductType.FOREIGN,
       company: '스카이필 단독상품',
-      image: 'https://picsum.photos/200',
+      image: 'assets/images/Rectangle1.png',
       price: 1042525,
       title: '[비즈니스/클래스] “백야! 한 밤의 태양을 즐겨라” 북유럽 4개국',
     },
     {
       type: ProductType.DOMESTIC,
       company: '광주 여행사',
-      image: 'https://picsum.photos/200',
+      image: 'assets/images/Rectangle2.png',
       price: 9202100,
       title:
         '[정통][래디슨][오션뷰X마사지UP][#바나힐 #한시장 #스테이크]▶다낭,호이안...',
@@ -30,7 +36,7 @@ export default class IndexPage {
     {
       type: ProductType.FOREIGN,
       company: '스카이필 단독상품',
-      image: 'https://picsum.photos/200',
+      image: 'assets/images/Rectangle3.png',
       price: 2199000,
       title:
         "[추석연휴 특별기획][대한항공][가을낭만 '특급료칸♨+식도락'] 오카야마&돗토...",
@@ -38,10 +44,27 @@ export default class IndexPage {
     {
       type: ProductType.FOREIGN,
       company: '스카이필 단독상품',
-      image: 'https://picsum.photos/200',
+      image: 'assets/images/Rectangle4.png',
       price: 1899000,
       title:
         '[추석연휴 특별기획][대한항공 고마츠 직항][가을빛 수놓은 오색 단풍] 도야마...',
+    },
+  ];
+  contents: Content[] = [
+    {
+      image: 'assets/images/Rectangle5.png',
+      title: '일본 오사카 여행 온천 가족여행 4일',
+      subTitle: '지금 바로 다양한 헤택을 누려보세요!',
+    },
+    {
+      image: 'assets/images/Rectangle6.png',
+      title: '오사카 4일',
+      subTitle: '지금 바로 다양한 헤택을 누려보세요!',
+    },
+    {
+      image: 'assets/images/Rectangle7.png',
+      title: '오사카 4일',
+      subTitle: '지금 바로 다양한 헤택을 누려보세요!',
     },
   ];
 }

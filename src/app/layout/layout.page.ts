d75a9b4babecba51d26ca789af3d658ccfb9e02d
Anjh <anjh3325@gmail.com>
2023-09-11 15:46:08 +0900

    footer정렬

--- src/app/layout/layout.page.ts
+++ src/app/layout/layout.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../nav/nav.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.page.html',
   styleUrls: ['./layout.page.scss'],
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
 })
 export class LayoutPage {}

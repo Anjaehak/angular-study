9c8382fb00892c2def754a10634239b09cf224e7
Anjh <anjh3325@gmail.com>
2023-09-15 10:13:36 +0900

    수정 필요

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       boxShadow: {
         '3xl': '0px 4px 6px -2px rgba(0, 0, 0, 0.05), 0px 10px 15px -3px rgba(0, 0, 0, 0.10)',
-      }
+      },
     },
   },
   plugins: [],

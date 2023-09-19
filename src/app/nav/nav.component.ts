4e67e0d6b03a7a2197c8c1c4f8d53c8bb4aee92c
Anjh <anjh3325@gmail.com>
2023-09-19 18:27:26 +0900

    어렵다 반응형

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 type Nav = {
   name: string;
@@ -16,15 +16,22 @@ type Nav = {
 })
 export class NavComponent {
   navs: Nav[] = [
-    { name: '내나라여행', url: 'domesticTravel' },
-    { name: '해외여행', url: 'foreignTravel' },
-    { name: '견적요청', url: 'estimate' },
+    { name: '내나라여행', url: '/domesticTravel' },
+    { name: '해외여행', url: '/foreignTravel' },
+    { name: '견적요청', url: '/estimate' },
     { name: '관광콘텐츠', url: '' },
     { name: 'ghl', url: '' },
     { name: '고객센터', url: '' },
   ];
 
   selectedButton: number | null = null;
+
+  get path() {
+    console.log(this.router.url);
+    return this.router.url;
+  }
+
+  constructor(private router: Router) {}
 
   changeButtonColor(buttonId: number) {
     // 버튼을 클릭할 때 해당 버튼의 상태를 변경합니다.

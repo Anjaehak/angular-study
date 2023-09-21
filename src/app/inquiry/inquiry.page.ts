d993e8e02dd4ae79b6c15bdcbf2eaae141bf8ed0
Anjh <anjh3325@gmail.com>
2023-09-21 17:13:13 +0900

    문의내역 생성

---
+++ src/app/inquiry/inquiry.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inquiry',
+  templateUrl: './inquiry.page.html',
+  styleUrls: ['./inquiry.page.scss'],
+})
+export default class InquiryComponent {}

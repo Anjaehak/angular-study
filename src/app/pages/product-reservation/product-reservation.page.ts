f82cb93ab5c15c9b3a96b978e42fe5ed754764ef
Anjh <anjh3325@gmail.com>
2023-10-23 17:33:59 +0900

    10/23/1733

--- src/app/pages/product-reservation/product-reservation.page.ts
+++ src/app/pages/product-reservation/product-reservation.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectComponent } from 'src/app/components/select/select.component';
 
 @Component({
   selector: 'app-product-reservation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SelectComponent],
   templateUrl: './product-reservation.page.html',
   styleUrls: ['./product-reservation.page.scss'],
 })

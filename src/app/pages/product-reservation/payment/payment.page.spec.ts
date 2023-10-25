b3ab825690a3653dbab70c6fb39e76dd45f19728
Anjh <anjh3325@gmail.com>
2023-10-25 19:18:05 +0900

    payment

---
+++ src/app/pages/product-reservation/payment/payment.page.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaymentPage } from './payment.page';
+
+describe('PaymentPage', () => {
+  let component: PaymentPage;
+  let fixture: ComponentFixture<PaymentPage>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PaymentPage]
+    });
+    fixture = TestBed.createComponent(PaymentPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

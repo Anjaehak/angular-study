8ab9609acbd325c006d3350d0763e9280d111af9
Anjh <anjh3325@gmail.com>
2023-10-25 19:22:49 +0900

    initialPayment

--- src/app/pages/product-reservation/product-reservation.page.ts
+++ src/app/pages/product-reservation/product-reservation.page.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SelectComponent } from 'src/app/components/select/select.component';
 
 @Component({
   selector: 'app-product-reservation',
   standalone: true,
-  imports: [CommonModule, SelectComponent],
+  imports: [CommonModule, SelectComponent, RouterModule],
   templateUrl: './product-reservation.page.html',
   styleUrls: ['./product-reservation.page.scss'],
 })

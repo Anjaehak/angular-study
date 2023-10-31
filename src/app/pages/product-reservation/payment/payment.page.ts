50a9dd3c15fc04aea8dde9e8acc759b0aaefa65a
Anjh <anjh3325@gmail.com>
2023-10-31 20:20:57 +0900

    select

--- src/app/pages/product-reservation/payment/payment.page.ts
+++ src/app/pages/product-reservation/payment/payment.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectComponent } from 'src/app/components/select/select.component';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SelectComponent],
   templateUrl: './payment.page.html',
   styleUrls: ['./payment.page.scss'],
 })
